e7ce18e53ff1e16b38f5ee0839a5cddf897478d1
jayraj-parki <jayrajfd@gmail.com>
2024-08-25 20:55:57 +0530

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,8 +146,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={style.headerOuter2 + ` bg-info row col-12 py-4 p-0 m-0 mx-auto`}>
-          <div className={style.headerInner2 + ` row bg-info p-0 col-12 col-sm-10 m-0 mx-auto d-flex   justify-content-between `}>
+        <div className={style.headerOuter2 + `  row col-12  p-0 py-1 m-0 mx-auto`}>
+          <div className={style.headerInner2 + ` row  p-0 col-12 col-sm-10 m-0 mx-auto d-flex   justify-content-between `}>
             <div className={style.websiteName + ` col-auto p-0 m-0 `}>
               <h3 className="text-light">Join our telegram to make Big Profits</h3>
             </div>
