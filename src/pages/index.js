94bbbaee3deba9418d4bf66f0fcfff34683df3a7
jayraj-parki <jayrajfd@gmail.com>
2024-09-01 16:49:48 +0530

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -216,7 +216,7 @@ export default function Home() {
             </div>
             <div className="mb-auto  text-start">
               <p className="h5 pt-4  text-start mb-4 mb-sm-0 ">Join our <span className="text-info">Telegram Channel</span> for execlusive <span className="text-warning">Stock Market</span> expertise</p>
-              <small className="pt-3">Access real-time market breakdown, innovative trading strategis and deep - dive investment analysis. Join our Telegram Channel for free today.</small>
+              <small className="pt-3">Access real-time market breakdown, innovative trading strategies and deep - dive investment analysis. Join our Telegram Channel for free today.</small>
             </div>
             <div className={style.telegram + ` row col-auto col-md-12 mb-auto mx-auto mt-4 mt-xl-0`}>
               <Button onClick={()=>openNewURL('https://t.me/fireinvestingofficials')} className=" col-auto  col-md-8  px-3 mx-auto"><TelegramIcon className={style.TelegramIcon + "  mx-2 rounded-pill p-1"} /> Join our free Telegram</Button>
@@ -228,7 +228,7 @@ export default function Home() {
               <img className="d-block rounded-5 " src={'/images/logo.webp'} alt="..." />
             </div>
             <div className="row mb-auto mx-auto mt-4 mt-xl-0 ">
-              <p onClick={()=>openNewURL('https://t.me/fireinvestingofficials')} className={style.cert + " px-5 py-2 h2 rounded-2 mx-auto col-auto d-flex justify-content-center align-items-center"}><WorkspacePremiumIcon className="p-0  h3 m-0" />NISM Certified </p>
+              <Button onClick={()=>openNewURL('https://t.me/fireinvestingofficials')} className={style.cert + " px-5 py-2 h2 rounded-2 mx-auto col-auto d-flex justify-content-center align-items-center"}><WorkspacePremiumIcon className="p-0  h3 m-0" />NISM  <span className='text-capitalize'>Certified</span> </Button>
             </div>
           </div>
         </div>
